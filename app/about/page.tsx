9b94e3d2eb5da4445137d1e6229ea2c397738ca5
Sandun Tharuka <sandun.suntws@gmail.com>
2023-03-19 20:48:31 +0530

    setup lay out

---
+++ app/about/page.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+
+export default function About() {
+
+  //throw new Error('Not today')
+  return (
+    <div><h1>About</h1>
+      <Link href={'/'}>Link</Link>
+    </div>
+  )
+}
